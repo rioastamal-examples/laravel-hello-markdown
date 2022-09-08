3c41ae2d8f3c67144a34930c410461a9ce028484
Rio Astamal <me@rioastamal.net>
2022-09-08 06:29:38 +0000

    Add environment to the footer

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) | Env: {{ env('APP_ENV') }}
                     </div>
                 </div>
             </div>
